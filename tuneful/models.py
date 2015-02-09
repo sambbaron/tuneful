855bd8fb884b429649b54f8293ec5c44526a22fa
Sam Baron <sambaron@ownmail.net>
2015-02-09 21:09:38 +0000

    Models: create db tables

--- tuneful/models.py
+++ tuneful/models.py
@@ -7,3 +7,34 @@ from sqlalchemy.orm import relationship
 from tuneful import app
 from database import Base, engine
 
+class Song(Base):
+    __tablename__ = "song"
+    
+    id = Column(Integer, primary_key=True)
+    file_id = Column(Integer, ForeignKey('file.id'), nullable=False)
+    
+    def as_dictionary(self):
+        song = {
+            "id": self.id,
+            "name": self.file.name
+        }
+        return song
+
+
+class File(Base):
+    __tablename__ = "file"
+    
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128))
+    song = relationship("Song", uselist=False, backref="file")    
+        
+    def as_dictionary(self):
+        file = {
+            "id": self.id,
+            "name": self.name
+        }
+        return file
+    
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)    
+
