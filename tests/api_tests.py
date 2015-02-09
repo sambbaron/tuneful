66b7bc53d08fe47584b08648b03270f032a82e2e
Sam Baron <sambaron@ownmail.net>
2015-02-09 23:22:26 +0000

    API: get and post

--- tests/api_tests.py
+++ tests/api_tests.py
@@ -35,5 +35,123 @@ class TestAPI(unittest.TestCase):
 
         # Delete test upload folder
         shutil.rmtree(upload_path())
+        
+    def testGetSongs(self):
+        """ Get all songs from database and return JSON """
+        
+        # Add new songs
+        fileA = models.File(name="Test File 1")
+        songA = models.Song(file_id=1)
+        fileB = models.File(name="Test File 2")
+        songB = models.Song(file_id=2)
+        session.add_all([fileA, songA, fileB, songB])
+        session.commit()
+        
+        # Return response
+        response = self.client.get("/api/songs",
+            headers=[("Accept", "application/json")]
+        )
+        
+        # Test status code, mimetype, and number of rows
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.mimetype, "application/json")
+        data = json.loads(response.data)
+        self.assertEqual(len(data), 2)
+        songA = data[0]
+        self.assertEqual(songA["file"]["name"], "Test File 1")
+        songB = data[1]
+        self.assertEqual(songB["file"]["name"], "Test File 2")
 
+    def testGetSong(self):
+        """ Get single song from database and return JSON """
+        
+        # Add new songs
+        fileA = models.File(name="Test File 1")
+        songA = models.Song(file_id=1)
+        fileB = models.File(name="Test File 2")
+        songB = models.Song(file_id=2)
+        session.add_all([fileA, songA, fileB, songB])
+        session.commit()
+        
+        # Return response
+        response = self.client.get("/api/songs/2",
+            headers=[("Accept", "application/json")]
+        )
+        
+        # Test status code, mimetype, and number of rows
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.mimetype, "application/json")
+        song = json.loads(response.data)
+        self.assertEqual(song["file"]["name"], "Test File 2")
+        
+        
+    def testPostSong(self):
+        """ Add single song  """
+        
+        # Add file prior to testing song
+        file = models.File(name="Test Song")
+        session.add(file)
+        session.commit()
+        
+        # Song post data
+        song = {"file": {"id": 1}}
+        
+        response = self.client.post("/api/songs",
+            data = json.dumps(song),
+            content_type = "application/json",
+            headers = [("Accept", "application/json")]
+        )
+        
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.mimetype, "application/json")
+        self.assertEqual(urlparse(response.headers.get("Location")).path,
+                         "/api/songs/1")
+        
+        data = json.loads(response.data)
+        self.assertEqual(data["id"], 1)
+        self.assertEqual(data["file"]["name"], "Test Song")
+        
+        songs = session.query(models.Song).all()
+        song = songs[0]
+        self.assertEqual(song.id, 1)
+        self.assertEqual(song.file.name, "Test Song")
 
+    def testPostSongInvalidFile(self):
+        """ Add single song without proper file id """
+        
+        # Add file prior to testing song
+        file = models.File(name="Test Song")
+        session.add(file)
+        session.commit()
+        
+        # Song post data
+        song = {"file": {"id": "0"}}
+        
+        response = self.client.post("/api/songs",
+            data = json.dumps(song),
+            content_type = "application/json",
+            headers = [("Accept", "application/json")]
+        )
+        
+        self.assertEqual(response.status_code, 422)
+        data = json.loads(response.data)
+        
+    def testPostSongNoFile(self):
+        """ Add single song without file """
+        
+        # Add file prior to testing song
+        file = models.File(name="Test Song")
+        session.add(file)
+        session.commit()
+        
+        # Song post data
+        song = {"wrong": "data"}
+        
+        response = self.client.post("/api/songs",
+            data = json.dumps(song),
+            content_type = "application/json",
+            headers = [("Accept", "application/json")]
+        )
+        
+        self.assertEqual(response.status_code, 422)
+        data = json.loads(response.data)        
--- tuneful/api.py
+++ tuneful/api.py
@@ -11,4 +11,64 @@ from tuneful import app
 from database import session
 from utils import upload_path
 
+# JSON Schema describing the structure of a post
+song_schema = {
+    "properties": {
+        "file" : {
+            "properties": {
+                "id": {"type": "number"}    
+            },
+            "required": ["id"]
+        },
+    },
+    "required": ["file"]
+}
 
+@app.route("/api/songs", methods=["GET"])
+@decorators.accept("application/json")
+def songs_get():
+    """ Get a list of songs"""
+    
+    songs = session.query(models.Song).all()
+    data = json.dumps([song.as_dictionary() for song in songs])
+    return Response(data, 200, mimetype="application/json")
+
+@app.route("/api/songs/<int:id>", methods=["GET"])
+@decorators.accept("application/json")
+def song_get(id):
+    """ Return single song """
+    
+    song = session.query(models.Song).get(id)
+
+    if not song:
+        message = "Could not find song with id {}".format(id)
+        data = json.dumps({"message": message})
+        return Response(data, 404, mimetype="application/json")
+
+    data = json.dumps(song.as_dictionary())
+    return Response(data, 200, mimetype="application/json")
+
+@app.route("/api/songs", methods=["POST"])
+@decorators.accept("application/json")
+@decorators.require("application/json")
+def songs_post():
+    """ Add a new song"""
+    
+    data = request.json
+    
+    # Validate JSON against schema
+    try:
+        validate(data, song_schema)
+    except ValidationError as error:
+        data = {"message": error.message}
+        return Response(json.dumps(data), 422, mimetype="application/json")
+    
+    # Add song to database
+    song = models.Song(file_id=data["file"]["id"])
+    session.add(song)
+    session.commit()
+    
+    # Return JSON response and location header
+    data = json.dumps(song.as_dictionary())
+    headers = {"Location": url_for("song_get", id=song.id)}
+    return Response(data, 201, headers=headers, mimetype="application/json")
